20192ccf35900012bd8436c07996b3ae0a6e057d
wallliddd <wavzaly@gmail.com>
2023-03-06 20:08:18 +0100

    third test
    a

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editaa <code>src/App.js</code> and save to reload.
+          Editaaa <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
